89d685aeae7a84631a31bd9b4133bb54bbb3bbab
Avishek Chowdhury <zussyfreak@gmail.com>
2024-11-10 13:03:18 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+//sadkfjkasd
+const http = require('http');
+
+const server = createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello, World!\n');
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
